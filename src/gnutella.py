cca119782e694bb9e279cd49c7addc79fe80733e
luca <luca@ubuntu.ubuntu-domain>
2014-03-25 17:55:41 +0100

    inizio

---
+++ src/gnutella.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="luca"
+__date__ ="$25-mar-2014 17.54.16$"
+
+if __name__ == "__main__":
+    print "Hello World"
