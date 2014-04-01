83f0d63ac53d58dda210f17f3875f412edab6cbd
dghinato <dghinato@gmail.com>
2014-04-01 17:08:31 +0200

    Classe SearchResult aggiunta. ^_^

---
+++ src/SearchResult.py
@@ -0,0 +1,7 @@
+class SearchResult:
+    
+    def __init__(self, filename, filemd5, ipp2p, pp2p):
+        self.filename = filename
+        self.filemd5 = filemd5
+        self.ipp2p = ipp2p
+        self.pp2p = pp2p
